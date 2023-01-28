bd5a4078a58ee774444e7649f69b1aaf0545ace8
Tejas <Tejas@DESKTOP-3FS3IDM>
2023-01-28 23:27:29 +0530

    [Tejas]:2nd Modify

---
+++ src/Git_JAN/Class112.java
@@ -0,0 +1,10 @@
+package Git_JAN;
+
+public class Class112 
+{
+   public static void main(String[] args) 
+   {
+	   System.out.println("Tejas Hingnekar");
+	   System.out.println("Nagpur");
+   }
+}
